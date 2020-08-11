634d19340e2e1f8283e728ec431160285c967f57
AlekseyK <waltersob@meta.ua>
2020-08-11 20:29:41 +0300

    Deleted commented debug-stuff

--- trialProjApp/urls.py
+++ trialProjApp/urls.py
@@ -7,7 +7,6 @@ app_name = 'trial_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
-    # path('download/', views.download_csv, name='downloadCSV'),
     path('<str:meter_id>/', views.detail, name='detail'),
     path('<str:meter_id>/deleteAll/', views.delete_all, name='deleteAll'),
     path('<str:meter_id>/uploadData/', views.upload_data, name='uploadData'),
