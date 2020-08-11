0a0fac234bbe596f73970dba907f5d7ee5d31048
AlekseyK <waltersob@meta.ua>
2020-08-11 15:53:32 +0300

    Added test that checks data fed to charts

--- trialProjApp/tests.py
+++ trialProjApp/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase, Client
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from datetime import date
 from .models import Meter, MeterData
-from .utils import validation
+from .utils import validation, prepare_detail
+from .meterEntity import *
 
 
 # Create your tests here.
@@ -24,13 +26,15 @@ class TestViews(TestCase):
 
 class TestRW(TestCase):
 
+    error = ''
+
     @classmethod
-    def set_up_test_data(cls):
+    def setUpTestData(cls):
         cls.tested_meter = Meter.objects.create(name='Meter_1', resource_type='G', unit='Unit 1')
-        cls.test_meter_data_one = MeterData.objects.create(date=date(2015, 11, 2), value=10135.0, meter_id=cls.tested_meter)
-        cls.test_meter_data_two = MeterData.objects.create(date=date(2015, 10, 5), value=10072.0, meter_id=cls.tested_meter)
-        cls.test_meter_data_three = MeterData.objects.create(date=date(2015, 9, 3), value=10059.0, meter_id=cls.tested_meter)
-        cls.test_meter_data_four = MeterData.objects.create(date=date(2015, 8, 2), value=10049.0, meter_id=cls.tested_meter)
+        cls.test_meter_data_one = MeterData.objects.create(date=date(2015, 11, 2), value=11200.0, meter_id=cls.tested_meter.name)
+        cls.test_meter_data_two = MeterData.objects.create(date=date(2015, 10, 5), value=11170.0, meter_id=cls.tested_meter.name)
+        cls.test_meter_data_three = MeterData.objects.create(date=date(2015, 9, 3), value=11153.0, meter_id=cls.tested_meter.name)
+        cls.test_meter_data_four = MeterData.objects.create(date=date(2015, 8, 2), value=11100.0, meter_id=cls.tested_meter.name)
 
     def test_create(self):
         self.assertTrue(isinstance(self.tested_meter, Meter))
@@ -46,3 +50,21 @@ class TestRW(TestCase):
         }
         valid_response = validation(data)
         self.assertTrue(type(valid_response) is not str)
+
+    def test_check_integrity_values(self):
+        data_csv = open('trialProjApp/for_test.csv', 'r')
+        dat = []
+        for line in data_csv:
+            str_text = line
+            dat.append(str_text)
+        m = MeterEntity(name=self.tested_meter.name)
+        m.load_data(dat)
+        print(m.errors)
+        meter = get_object_or_404(Meter, pk=self.tested_meter.name)
+        meter_data_list = meter.meterdata_set.all().order_by('date')
+        ready_data = prepare_detail(meter_data_list)
+        graph_values = ready_data['xValues']
+        for dif in graph_values:
+            if dif < 0:
+                self.error = 'Data in graph contains negative difference'
+        self.assertEquals(self.error, '')
